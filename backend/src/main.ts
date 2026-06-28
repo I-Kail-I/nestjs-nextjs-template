c47a4dde036bc0c4691bb196dc202b329e3676e1
I-Kail-I <mikailgames121@gmail.com>
2026-06-28 21:26:51 +0800

    feat: reconfig the cors for prod
    now if it's in production if not using reverse proxy like for mobile app for example, you can put the url so it was the trusted origin, but in development it trust every origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT: number = Number(process.env.PORT ?? 8000);
+
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'production' ? process.env.FRONTEND_URL : '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   app.setGlobalPrefix('api');
   app.use(helmet());
 
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(PORT);
+  console.log(`Server is running on port ${PORT}`);
 }
 bootstrap();
