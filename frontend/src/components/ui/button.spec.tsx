6b9427970806746a7942bc526761597b1f5b9f71
I-Kail-I <mikailgames121@gmail.com>
2026-07-09 12:21:55 +0800

    chore: adding all components test

--- frontend/src/components/ui/button.spec.tsx
+++ frontend/src/components/ui/button.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Button } from '@/components/ui/button';
+import { Button } from './button';
 
 describe('Button', () => {
   it('renders children', () => {
